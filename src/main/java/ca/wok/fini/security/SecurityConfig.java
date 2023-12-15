baf23996a39679dd66fb12c0e69020816bff5ee0
Alex Singleton <97618579+Sixsinglebird@users.noreply.github.com>
2023-12-15 03:22:52 -0330

    |_^_| - comment update

--- src/main/java/ca/wok/fini/security/SecurityConfig.java
+++ src/main/java/ca/wok/fini/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 import javax.sql.DataSource;
 
@@ -31,7 +32,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+        // csrf -> csrf.csrfTokenRepository(new HttpSessionCsrfTokenRepository())
         http
                 .csrf().disable()
                 .authorizeHttpRequests(authorize -> authorize.requestMatchers("/airport").permitAll())
